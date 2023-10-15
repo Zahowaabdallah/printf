44b259ceb3c197be5c75db270a976f318a0def8f
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-15 14:00:09 -0700

    a

--- print_rot13.c
+++ print_rot13.c
@@ -33,11 +33,12 @@ void convertToRot13(char *str, int *count)
 	len = _strlen(str);
 	for (i = 0; i < len; i++)
 	{
-		if ((str[i] >= 'A' && str[i] <= 'M' || str[i] >= 'a' && str[i] <= 'm'))
+		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
 		{
 			_putchar(str[i] + 13);
 		}
-		else if ((str[i] >= 'N' && str[i] <= 'Z' || str[i] >= 'n' && str[i] <= 'z'))
+		else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n'
+			&& str[i] <= 'z'))
 		{
 			_putchar(str[i] - 13);
 		}
