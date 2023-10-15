64a63d240a57a29c557de649756924bc7a0397bc
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-15 07:46:25 -0700

    a

--- main.h
+++ main.h
@@ -10,5 +10,6 @@ int _putchar(char ch);
 void print_char(char ch, int *count);
 void print_string(char *str, int *count);
 void print_integer(long int number, int *count);
+void convertToRot13(char *str, int *count);
 
 #endif
