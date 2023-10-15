42fbf8c9c0be802380532eb05a49026705279e66
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-15 04:19:02 -0700

    a

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char ch);
+void print_char(char ch, int *count);
+void print_string(char *str, int *count);
+void print_integer(int number, int *count);
+
+#endif
