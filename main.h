2cdeec205ce0f7fbce388353f2208e40312c3f05
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-16 08:48:38 -0700

    print_binary.c

--- main.h
+++ main.h
@@ -14,4 +14,6 @@ void print_integer(long int number, int *count);
 void convertToRot13(char *str, int *count);
 void print_reverse_string(char *str, int *count);
 void find_conversion_specifier(char format, va_list list, int *count);
+void print_binary(int number, int *count);
+
 #endif
